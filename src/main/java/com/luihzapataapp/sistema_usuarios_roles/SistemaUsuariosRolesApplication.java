35a17fc43b70603b3b95c97b283558d3a61477c9
primer_proyecto <luis.zapata5@udea.edu.co>
2026-05-16 22:53:29 -0500

    configuracion de la app properties para conectarla con la base de datos y protecion de la base de datos

---
+++ src/main/java/com/luihzapataapp/sistema_usuarios_roles/SistemaUsuariosRolesApplication.java
@@ -0,0 +1,13 @@
+package com.luihzapataapp.sistema_usuarios_roles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaUsuariosRolesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaUsuariosRolesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luihzapataapp/sistema_usuarios_roles/SistemaUsuariosRolesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luihzapataapp.sistema_usuarios_roles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaUsuariosRolesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
