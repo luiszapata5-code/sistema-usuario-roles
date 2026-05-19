fb739be670ca67b0c484c36b4e9008de578b6296
primer_proyecto <luis.zapata5@udea.edu.co>
2026-05-19 17:59:46 -0500

    se crearon los repositorios de las entidades

---
+++ src/main/java/com/luihzapataapp/sistema_usuarios_roles/repositories/Usuario_repositorio.java
@@ -0,0 +1,10 @@
+package com.luihzapataapp.sistema_usuarios_roles.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.luihzapataapp.sistema_usuarios_roles.model.Usuario;
+@Repository
+public interface Usuario_repositorio extends JpaRepository<Usuario, Integer> {
+    
+}
